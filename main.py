f2bdd64df9284c6533fecdbcbb37dabdf6b34eb8
Gaurav Baliyan <gauravbaliyan@Gauravs-MacBook-Pro.local>
2026-02-11 22:32:24 +0530

    Updated code for new api

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from mangum import Mangum
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    arg1: str
+    arg2: str
+
 @app.get("/")
 def root():
-    return {"message": "Updated: FastAPI running on Lambda via Docker 🚀"}
+    return {"message": "Updated: FastAPI running on Lambda via Docker latest"}
+
+@app.post("/post-data")
+def print_args(item: Item):
+    print(f"arg1: {item.arg1}, arg2: {item.arg2}")
+    return {"arg1": item.arg1, "arg2": item.arg2}
 
 handler = Mangum(app)
